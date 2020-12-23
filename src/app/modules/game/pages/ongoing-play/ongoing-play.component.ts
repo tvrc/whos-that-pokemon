8e4f26127dcd134866f63178211e655ff56d8e00
Tomasz Waręcki <tomasz.warecki.franczak@gmail.com>
2020-12-23 21:43:36 +0100

    timer, navigation after end time

--- src/app/modules/game/pages/ongoing-play/ongoing-play.component.ts
+++ src/app/modules/game/pages/ongoing-play/ongoing-play.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
+import { finalize, takeWhile, tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ongoing-play',
   templateUrl: './ongoing-play.component.html',
   styleUrls: ['./ongoing-play.component.scss']
 })
-export class OngoingPlayComponent implements OnInit {
+export class OngoingPlayComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  timer = 10;
+  private timerSub: Subscription;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.setupTimer();
   }
 
+  private setupTimer(): void {
+    this.timerSub = interval(1000)
+      .pipe(
+        tap(() => --this.timer),
+        takeWhile(() => this.timer > 0),
+        finalize(() => this.router.navigate(['results']))
+      ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.timerSub.unsubscribe();
+  }
 }
